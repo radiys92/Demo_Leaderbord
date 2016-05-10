0ae7236127a1c063675c3186ef8f04ebf00463d7
Radomyr <radomyrslaboshpitskyi@gmail.com>
2016-05-11 01:46:56 +0300

    final header and filters layout

---
+++ LeaderboardsUnityProj/Assets/Scripts/GUI/Editor/GoToggleEditor.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEditor;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CustomEditor(typeof (GoToggle), true)]
+[CanEditMultipleObjects]
+public class GoToggleEditor : ToggleEditor
+{
+    private SerializedProperty m_OnValueChangedProperty;
+    private SerializedProperty m_TransitionProperty;
+    private SerializedProperty m_GraphicProperty;
+    private SerializedProperty m_GroupProperty;
+    private SerializedProperty m_IsOnProperty;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        this.m_TransitionProperty = this.serializedObject.FindProperty("toggleTransition");
+        this.m_GraphicProperty = this.serializedObject.FindProperty("_TargetGo");
+        this.m_GroupProperty = this.serializedObject.FindProperty("m_Group");
+        this.m_IsOnProperty = this.serializedObject.FindProperty("m_IsOn");
+        this.m_OnValueChangedProperty = this.serializedObject.FindProperty("onValueChanged");
+    }
+
+    /// <summary>
+    /// 
+    /// <para>
+    /// See Editor.OnInspectorGUI.
+    /// </para>
+    /// 
+    /// </summary>
+    public override void OnInspectorGUI()
+    {
+        this.serializedObject.Update();
+        EditorGUILayout.PropertyField(this.m_IsOnProperty);
+        EditorGUILayout.PropertyField(this.m_TransitionProperty);
+        EditorGUILayout.PropertyField(this.m_GraphicProperty);
+        EditorGUILayout.PropertyField(this.m_GroupProperty);
+        EditorGUILayout.Space();
+        EditorGUILayout.PropertyField(this.m_OnValueChangedProperty);
+        this.serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ LeaderboardsUnityProj/Assets/Scripts/GUI/GoToggle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GoToggle : Toggle
+{
+    [SerializeField]
+    private GameObject _TargetGo;
+
+    protected override void OnValidate()
+    {
+        base.OnValidate();
+        UpdateState();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        if (_TargetGo != null)
+            onValueChanged.AddListener(_TargetGo.SetActive);
+    }
+
+    private void UpdateState()
+    {
+        if (_TargetGo != null)
+            _TargetGo.SetActive(isOn);
+    }
+}
